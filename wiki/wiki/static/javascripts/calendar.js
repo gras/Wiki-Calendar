b707b2177a47d9dc9c8a51fd427a2bd12bed9ded
graysonelias <graysonelias@gmail.com>
2017-06-07 00:59:31 -0400

    Moved CSS and JS into their own files.

---
+++ wiki/wiki/static/javascripts/calendar.js
@@ -0,0 +1,11 @@
+function select_name(name) {
+    document.getElementById("name_box").value=name;
+    }
+
+function select_date(date) {
+    document.getElementById("date_box").value=date;
+    }
+
+function select_entry(text) {
+    document.getElementById("entry_box").value=text;
+    }
